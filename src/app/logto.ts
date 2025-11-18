c34f13490123e7e91a44de2dcae767c9d6c0ac64
Edgar de la Cruz <edgardelacruz@MacBook-Air-de-Edgar.local>
2025-11-18 16:09:27 +0100

    Ultimos cambios 2

--- src/app/logto.ts
+++ src/app/logto.ts
@@ -4,7 +4,7 @@ export const logtoServerConfig = {
   endpoint: process.env.NEXT_PUBLIC_LOGTO_ENDPOINT!,
   appId: process.env.NEXT_PUBLIC_LOGTO_APP_ID!,
   appSecret: process.env.LOGTO_APP_SECRET!,
-  baseUrl: process.env.NEXT_PUBLIC_BASE_URL!,
+  baseUrl: process.env.LOGTO_BASE_URL!,
   cookieSecret: process.env.LOGTO_COOKIE_SECRET!,
   cookieSecure: process.env.NODE_ENV === "production",
 };
