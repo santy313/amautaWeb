04b46444e867a03def284fa052fc8fff84997144
Edgar de la Cruz <edgardelacruz@MacBook-Air-de-Edgar.local>
2025-11-18 18:10:31 +0100

    Ultimos cambios 5

--- src/app/callback/route.ts
+++ src/app/callback/route.ts
@@ -1,17 +1,10 @@
 import { handleSignIn } from '@logto/next/server-actions';
 import { redirect } from 'next/navigation';
 import { NextRequest } from 'next/server';
-import { logtoServerConfig } from '../../../lib/logto';
-
-
+import { logtoServerConfig } from '@/lib/logto';
 
 export async function GET(request: NextRequest) {
-  
   const searchParams = request.nextUrl.searchParams;
-
-  console.log('searchParams: ',searchParams);
-
-  await handleSignIn(logtoServerConfig, searchParams);
-
+  await handleSignIn(logtoServerConfig, searchParams); 
   redirect('/dashboard/main');
 }
